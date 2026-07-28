efa2bed362a0441be8cefeb4d3bb7db955d53053
Robbie Lyman <rb.lymn@gmail.com>
2026-07-28 14:23:31 -0400

    fix(math): don't overflow on mobile

--- assets/render-mathtex.js
+++ assets/render-mathtex.js
@@ -2,7 +2,8 @@ let eqns = document.querySelectorAll("script[type='math/tex']");
 for (let i=eqns.length-1; i>=0; i--) {
     let eqn = eqns[i];
     let src = eqn.text;
-    let d = eqn.closest('p') == null; 
-    eqn.outerHTML = temml.renderToString(src, { displayMode: d });
+    let d = eqn.closest('p') == null;
+    const mathString = temml.renderToString(src, { displayMode: d, wrap: "tex" });
+    eqn.outerHTML = d ? `<div class="math-block">${mathString}</div>` : mathString;
 }
 
